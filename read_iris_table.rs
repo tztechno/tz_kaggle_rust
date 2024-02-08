b8b1080a16ace5d488f911484e3d81b351ab2299
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-08 15:03:45 +0900

    Update read_iris_table.rs

--- read_iris_table.rs
+++ read_iris_table.rs
@@ -6,12 +6,10 @@ use csv::ReaderBuilder;
 use prettytable::{Table, Row, Cell};
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     let file_path = "iris.csv";
     let file = File::open(file_path)?;
     let mut rdr = ReaderBuilder::new().from_reader(file);
     let mut table = Table::new();
-
     for result in rdr.records() {
         let record = result?;
         let cells: Vec<Cell> = record.iter().map(|field| Cell::new(field)).collect();
