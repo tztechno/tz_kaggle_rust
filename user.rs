8713cf25b6c252006753595eac025f0ccefe8bfc
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-12 02:56:17 +0900

    Update user.rs

--- user.rs
+++ user.rs
@@ -12,7 +12,24 @@ use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 
 
-
+[dependencies]
+pyo3 = {version = "*", features = ["extension-module"]}
+pyo3-polars = {version = "0.11", features = ["derive"]}
+polars = {version = "0.37", features = ["performant", "lazy", "dtype-array", "ndarray","log", "cutqcut","nightly"]}
+num = "0.4.1"
+faer = {version = "0.16", features = ["ndarray", "nightly"]}
+serde = {version = "*", features=["derive"]}
+ndarray = {version="0.15.6", features=["rayon"]}
+hashbrown = {version = "0.14.2", features=["nightly"]}
+itertools = "0.12.0"
+rand = {version = "0.8.5"} # Simd support feature seems to be broken atm
+rand_distr = "0.4.3"
+realfft = "3.3.0"
+rapidfuzz = "0.5.0"
+inflections = "1.1.1"
+kdtree = {git = "https://github.com/mrhooray/kdtree-rs.git"}
+petgraph = "0.6.4"
+ordered-float = "4.2.0"
 
 
 
