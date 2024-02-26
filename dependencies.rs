efc8c4d701297b8ac52752d8e19a89dc220d81b7
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-26 13:55:31 +0900

    Create dependencies.rs

---
+++ dependencies.rs
@@ -0,0 +1,9 @@
+
+
+
+
+plotters = "0.3.5"
+csv = "1.1.6"
+polars = "0.14.7"
+polars-core = {version = "0.14.7", features=["ndarray"]}
+smartcore = { version = "0.2.0", default-features = false, features=["nalgebra-bindings", "ndarray-bindings", "datasets"]}
